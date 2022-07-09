d0acb6a89caf515db277ed80f40f44a79f468e2b
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-07-09 21:33:54 +0530

    log console

--- index.js
+++ index.js
@@ -16,11 +16,11 @@
 5 : "bigint"
 6 : "symbol"
 
-// console.log(10+"20");
+console.log(10+"20");
 
-// console.log(9-"4");//bug
+console.log(9-"4");//bug
 
-// console.log("Java "+"Script"); 
+console.log("Java "+"Script"); 
 
 console.log("Java "-"Script"); // NaN
 
