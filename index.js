c295d6384e0d585d9ed0a28b8c54c5fb57bc24a4
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-07-17 23:24:14 +0530

    findindex

--- index.js
+++ index.js
@@ -214,7 +214,12 @@
 //filtration in javascript
 
 
+// const prices = [200, 300, 350, 400, 500, 600];
+// //price<400
+// const findElem = prices.find((currentValue) => currentValue < 400);
+// console.log(findElem);
+
 const prices = [200, 300, 350, 400, 500, 600];
 //price<400
-const findElem = prices.find((currentValue) => currentValue < 400);
+const findElem = prices.findIndex((currentValue) => currentValue < 400);
 console.log(findElem);
